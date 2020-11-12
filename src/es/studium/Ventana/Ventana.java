f584dc0e1d35b1fd91574f87367aa40c2614dd67
Moisés Adamuz <moises.adamuz.fernandez@gmail.com>
2020-11-12 14:44:00 +0100

    Aplicación totalmente funcioanl al 100%

--- src/es/studium/Ventana/Ventana.java
+++ src/es/studium/Ventana/Ventana.java
@@ -18,6 +18,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.awt.event.ActionEvent;
 import java.awt.Component;
 import javax.swing.Box;
@@ -28,11 +29,18 @@ import javax.swing.JScrollPane;
 import java.awt.TextArea;
 import javax.swing.JList;
 import java.awt.Color;
+import javax.swing.JTable;
+import javax.swing.table.DefaultTableModel;
 
 public class Ventana extends JFrame  {
+	
+	//En estas variables guardaré el Nombre del proceso y el PID del proceso
+	String pidJuego = "";
+	String pidGestion = "";
 
 	private JPanel contentPane;
 	private JTextField txtCMD;
+	private JTable table;
 
 	/**
 	 * Launch the application.
@@ -72,10 +80,14 @@ public class Ventana extends JFrame  {
 		/*********************************************************************/
 		JTextArea textArea = new JTextArea();
 		//textArea.setBounds(21, 53, 211, 184);
+		//Añadimos el Scroll al textArea1
 		JScrollPane sp = new JScrollPane(textArea);
 		sp.setBounds(21, 53, 230, 184);
+		//Con setEditable, evitamos que se puedan realizar modificaciones en el textArea, es de solo lectura
 		textArea.setEditable(false);
 		contentPane.add(sp);
+		
+		
 		
 		/*********************************************************************************************/
 		///////////////////////Boton Ejecutar/////////////////////////////////////////////////////////
@@ -84,22 +96,20 @@ public class Ventana extends JFrame  {
 		btnCMD.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent ae) {
 				try {
-					
+					//Obtenemos lo que hemos escrito en el txtCMD para poder realizar la ejecución en el botón Ejecutar
 					String cmd = txtCMD.getText();
 					Process process = Runtime.getRuntime().exec("cmd /c " +cmd);
+					//Obtenemos el resultado del cmd y lo guardamos para su lectura
 				    InputStreamReader entrada = new InputStreamReader(process.getInputStream());
 				    BufferedReader br = new BufferedReader(entrada);
 				    String salida = null;
-				   
-		            	System.out.println("Comando ejecutado Correctamente");
+		            //System.out.println("Comando ejecutado Correctamente");
+				        //Leemos el contenido obtenido al ejecutar el comando
 		            	while ((salida=br.readLine()) != null){
-		                	textArea.append(salida+"\n");;
-		                	br.readLine();
-		                	System.out.println(salida);
+		                	textArea.append(salida+"\n");
+		                	//System.out.println(salida);
 		                }
 		           
-					
-					
 				    } 
 				catch (IOException ioe) {
 					System.out.println (ioe);
@@ -110,19 +120,21 @@ public class Ventana extends JFrame  {
 		btnCMD.setBounds(164, 21, 89, 23);
 		contentPane.add(btnCMD);
 		
-		/****************************************************************************/
-		////////////////////////////////Lista////////////////////////////////////////
-		/****************************************************************************/
-		JList lista = new JList();
-		lista.setBounds(289, 170, 138, 110);
-		contentPane.add(lista);
-		DefaultListModel modelo = new DefaultListModel();
-		//Agregamos modelo a la lista
-		lista.setModel(modelo);
-		//Seleccionamos el elemento de la lista
-		lista.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+		/***********************************************************/
+		/////////////////////Tabla//////////////////////////////////
+		/***********************************************************/
+		table = new JTable();
 		
-		
+		table.setModel(new DefaultTableModel(
+			new Object[][] {
+			},
+			new String[] {
+				"New column"
+			}
+		));
+		table.setBounds(289, 178, 124, 78);
+		DefaultTableModel modelo = (DefaultTableModel) table.getModel();
+		contentPane.add(table);
 	
 		
 		/***********************************************************/
@@ -136,17 +148,12 @@ public class Ventana extends JFrame  {
 				{
 					//https://stackoverrun.com/es/q/791304
 					Process process = Runtime.getRuntime().exec("notepad.exe");
-					
-				
-						modelo.addElement("Bloc de Notas"+"\n");
-						btnBlocNotas.setEnabled(false);
-						System.out.println("El Bloc de Notas está abierto");
-					
-					
-
-				
-				
-					
+					//Añadimos contenido a la fila
+					modelo.addRow(new Object[] {"Bloc de Notas"});
+					//System.out.println("Bloc de notas"+"//"+process.pid());
+					//Bloqueamos el botón
+					btnBlocNotas.setEnabled(false);
+					//System.out.println("El Bloc de Notas está abierto");
 										
 				}
 				catch (IOException ex)
@@ -169,16 +176,10 @@ public class Ventana extends JFrame  {
 				{
 					//https://stackoverrun.com/es/q/791304
 					Process process = Runtime.getRuntime().exec("mspaint.exe");
-					
-				if(process.isAlive())
-				{
-				
-					modelo.addElement("Paint"+"\n");
+					modelo.addRow(new Object[] {"Paint"});
+					//System.out.println("Paint"+"//"+process.pid());
 					btnPaint.setEnabled(false);
-					System.out.println("Paint está abierto");
-
-				}
-					
+					//System.out.println("Paint está abierto");	
 										
 				}
 				catch (IOException ex)
@@ -207,17 +208,11 @@ public class Ventana extends JFrame  {
 					String[] param = { arg1, arg2, arg3 };
 					//https://stackoverrun.com/es/q/791304
 					Process process = Runtime.getRuntime().exec(param);
-					
-				if(process.isAlive())
-				{
-				
-					modelo.addElement("Gestión"+"\n");
+					modelo.addRow(new Object[] {"Gestión"});	
+					//System.out.println("Gestión"+"//"+process.pid());
 					btnGestion.setEnabled(false);
-					System.out.println("El Programa de Gestión está abierto");
-
-				}
-					
-										
+					//System.out.println("El Programa de Gestión está abierto");	
+					pidGestion = "Gestión"+"//"+process.pid();
 				}
 				catch (IOException ex)
 				{
@@ -245,16 +240,12 @@ public class Ventana extends JFrame  {
 					String[] param = { arg1, arg2, arg3 };
 					//https://stackoverrun.com/es/q/791304
 					Process process = Runtime.getRuntime().exec(param);
-					
-				if(process.isAlive())
-				{
-				
-					modelo.addElement("Juego"+"\n");
+					modelo.addRow(new Object[] {"Juego"});
+					//System.out.println("Juego"+"//"+process.pid());
+					pidJuego = "Juego"+"//"+process.pid();
+					//System.out.println(pidJuego);
 					btnJuego.setEnabled(false);
-					System.out.println("El Juego está abierto");
-
-				}
-					
+					//System.out.println("El Juego está abierto");	
 										
 				}
 				catch (IOException ex)
@@ -280,10 +271,74 @@ public class Ventana extends JFrame  {
 		btnTerminar.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent ae) 
 			{
-				
-				int index = lista.getSelectedIndex();
-				
-				modelo.removeElementAt(index);
+				//En la variable entera guardo la posición de la fila seleccionada
+				int index = table.getSelectedRow();
+				//El valor de la fila lo convieto en un String || Indicamos que el valor de la columna es 0
+				String nombrePrg = table.getValueAt(index, 0).toString();
+				if (nombrePrg=="Bloc de Notas") {
+					
+					try {
+						//Ejecuto el cierre de la ventana
+						Process cerrarBN = Runtime.getRuntime().exec("cmd /c taskkill /f /im notepad.exe");
+						Thread.sleep(100);
+						btnBlocNotas.setEnabled(true);
+
+					} catch (IOException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					} catch (InterruptedException e1) {
+						// TODO Auto-generated catch block
+						e1.printStackTrace();
+					}
+				}
+				if (nombrePrg=="Paint") {
+					
+					try {
+						Process cerrarP = Runtime.getRuntime().exec("cmd /c taskkill /f /im mspaint.exe");
+						Thread.sleep(100);
+						btnPaint.setEnabled(true);
+
+					} catch (IOException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					} catch (InterruptedException e1) {
+						// TODO Auto-generated catch block
+						e1.printStackTrace();
+					}
+				}
+				if (nombrePrg=="Gestión") {
+					try {
+						//Ejecuto el cierre de la ventana, especificando el PID
+						Process cerrarG = Runtime.getRuntime().exec("cmd /c taskkill /f /pid "+ pidGestion.split("//")[1]);
+						//System.out.println("cmd /c taskkill /f /pid "+ pidGestion.split("//")[1]);
+						Thread.sleep(100);
+						btnGestion.setEnabled(true);
+					} catch (IOException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					} catch (InterruptedException e1) {
+						// TODO Auto-generated catch block
+						e1.printStackTrace();
+					}
+				}
+				if (nombrePrg=="Juego") {
+					
+					try {
+						
+						Process cerrarJ = Runtime.getRuntime().exec("cmd /c taskkill /f /pid "+ pidJuego.split("//")[1]);
+						//System.out.println("cmd /c taskkill /f /pid "+ pidJuego.split("//")[1]);
+						Thread.sleep(100);
+						btnJuego.setEnabled(true);
+
+					} catch (IOException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					} catch (InterruptedException e1) {
+						// TODO Auto-generated catch block
+						e1.printStackTrace();
+					}
+				}
+				modelo.removeRow(index);
 				
 				
 				
@@ -292,6 +347,8 @@ public class Ventana extends JFrame  {
 		});
 		btnTerminar.setBounds(437, 179, 89, 23);
 		contentPane.add(btnTerminar);
+		
+		
 		
 	
 		
